3557bda3a49fc14c57c74f84ace175117d0a9cd1
paul-nsc <40278548+paul-nsc@users.noreply.github.com>
2019-05-24 14:33:19 -0500

    Connection info with the api_key obfuscated

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+$api_key = "##############";
+$password = "x";
+$yourdomain = "northshorecare";
+
+define(API_KEY, $api_key);
+define(API_PW, $password);
+define(API_DOMAIN, $yourdomain);
+
+
